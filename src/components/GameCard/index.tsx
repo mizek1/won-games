f6751d3f18483d7e7af159b0fd59c6958b255b1d
mizek1 <dannalves111@gmail.com>
2023-08-06 14:36:03 -0300

    feat: gamecard component

---
+++ src/components/GameCard/index.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @next/next/no-img-element */
+import * as S from './styles'
+import { Favorite, FavoriteBorder } from 'styled-icons/material-outlined'
+import Button from 'components/Button'
+import { AddShoppingCart } from 'styled-icons/material'
+
+export type GameCardProps = {
+  title: string
+  developer: string
+  img: string
+  price: string
+  promotionalPrice?: string
+  favorite?: boolean
+  onFav?: () => void
+}
+
+const GameCard = ({
+  developer,
+  img,
+  price,
+  title,
+  promotionalPrice,
+  onFav,
+  favorite = false
+}: GameCardProps) => (
+  <S.Wrapper>
+    <S.ImageBox>
+      <img src={img} alt={title} />
+    </S.ImageBox>
+    <S.Content>
+      <S.Info>
+        <S.Title>{title}</S.Title>
+        <S.Developer>{developer}</S.Developer>
+      </S.Info>
+      <S.FavButton role="button" onClick={onFav}>
+        {favorite ? (
+          <Favorite aria-label="Remove from wishlist" />
+        ) : (
+          <FavoriteBorder aria-label="Add to wishlist" />
+        )}
+      </S.FavButton>
+      <S.BuyBox>
+        {!!promotionalPrice && <S.Price isPromotional>{price}</S.Price>}
+        <S.Price>{promotionalPrice || price}</S.Price>
+        <Button icon={<AddShoppingCart />} size="small"></Button>
+      </S.BuyBox>
+    </S.Content>
+  </S.Wrapper>
+)
+
+export default GameCard
