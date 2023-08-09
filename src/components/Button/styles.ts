9d0b059252dc9a2d19a6829c9189b71de3b219e4
mizek1 <dannalves111@gmail.com>
2023-08-09 19:14:28 -0300

    feat: minimal button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,8 @@
 import styled, { css, DefaultTheme } from 'styled-components'
 import { ButtonProps } from '.'
+import { darken } from 'polished'
 
-type WrapperProps = Pick<ButtonProps, 'size' | 'fullWidth'> & {
+type WrapperProps = Pick<ButtonProps, 'size' | 'fullWidth' | 'minimal'> & {
   hasIcon: boolean
 }
 
@@ -31,11 +32,19 @@ const wrapperModifiers = {
         margin-left: ${theme.spacings.xxsmall};
       }
     }
+  `,
+  minimal: (theme: DefaultTheme) => css`
+    background: none;
+    color: ${theme.colors.primary};
+
+    &:hover {
+      color: ${darken(0.1, theme.colors.primary)};
+    }
   `
 }
 
 export const Wrapper = styled.button<WrapperProps>`
-  ${({ theme, size, fullWidth, hasIcon }) => css`
+  ${({ theme, size, fullWidth, hasIcon, minimal }) => css`
     font-family: ${theme.font.family};
     background: linear-gradient(
       178.59deg,
@@ -54,11 +63,14 @@ export const Wrapper = styled.button<WrapperProps>`
     text-decoration: none;
 
     &:hover {
-      background: linear-gradient(180deg, #e35565 0%, #d958a6 50%);
+      background: ${minimal
+        ? 'none'
+        : 'linear-gradient(180deg, #e35565 0%, #d958a6 50%)'};
     }
 
     ${!!size && wrapperModifiers[size](theme)}
     ${fullWidth && wrapperModifiers.fullWidth()}
     ${hasIcon && wrapperModifiers.withIcon(theme)}
+    ${minimal && wrapperModifiers.minimal(theme)}
   `}
 `
