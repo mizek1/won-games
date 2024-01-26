009ddf2ab43373e95584f2b16d8157767f479f97
mizek1 <dannalves111@gmail.com>
2024-01-26 15:50:27 -0300

    feat: gamedetails component

---
+++ src/components/GameDetails/stories.tsx
@@ -0,0 +1,41 @@
+import { Story, Meta } from '@storybook/react'
+import GameDetails, { GameDetailsProps } from '.'
+import mockGame from './mock'
+
+export default {
+  title: 'Game/GameDetails',
+  component: GameDetails,
+  args: mockGame,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  argTypes: {
+    releaseDate: {
+      control: 'date'
+    },
+    platforms: {
+      control: {
+        type: 'inline-check',
+        options: ['windows', 'linux', 'mac']
+      }
+    },
+    genres: {
+      control: {
+        type: 'inline-check',
+        options: ['Role-playing', 'Narrative']
+      }
+    }
+  }
+} as Meta
+
+export const Default: Story<GameDetailsProps> = (args) => (
+  <div style={{ width: '130rem', margin: '0 auto' }}>
+    <GameDetails {...args} />
+  </div>
+)
+
+Default.args = {
+  platforms: ['windows', 'linux', 'mac']
+}
