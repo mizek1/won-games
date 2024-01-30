f38e62560b13fe0d52b88812fa65edbb3f66be6d
mizek1 <dannalves111@gmail.com>
2024-01-30 19:13:34 -0300

    feat: add button to cartlist

--- src/components/CartList/test.tsx
+++ src/components/CartList/test.tsx
@@ -13,4 +13,10 @@ describe('<CartList />', () => {
     expect(screen.getByText('R$ 330,00')).toHaveStyle({ color: '#F231A5' })
     expect(container.firstChild).toMatchSnapshot()
   })
+
+  it('should render the button', () => {
+    renderWithTheme(<CartList items={mockItems} total="R$ 330,00" hasButton />)
+
+    expect(screen.getByText(/buy it now/i)).toBeInTheDocument()
+  })
 })
