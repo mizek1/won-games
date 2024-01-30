782f90eac69141677dd2958b9d247afbd5d8d1a0
mizek1 <dannalves111@gmail.com>
2024-01-30 16:22:56 -0300

    feat: formprofile component

---
+++ src/components/FormProfile/stories.tsx
@@ -0,0 +1,12 @@
+import FormProfile from '.'
+
+export default {
+  title: 'Form/FormProfile',
+  component: FormProfile
+} as Meta
+
+export const Default: Story = () => (
+  <div style={{ maxWidth: 860, margin: 'auto' }}>
+    <FormProfile />
+  </div>
+)
