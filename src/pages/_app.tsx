5d76f2dae10a9e0c50cda68fed2fa3212fdb1f65
mizek1 <dannalves111@gmail.com>
2024-05-29 14:30:03 -0300

    feat: create useWishlist and wishlist provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import { ApolloProvider } from '@apollo/client'
-import { CartProvider } from 'hooks/use-cart'
+import NextNprogress from 'nextjs-progressbar'
+
 import { Provider as AuthProvider } from 'next-auth/client'
+import { ApolloProvider } from '@apollo/client'
+import { ThemeProvider } from 'styled-components'
+import { CartProvider } from 'hooks/use-cart'
+
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import NextNprogress from 'nextjs-progressbar'
-import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
 import { useApollo } from 'utils/apollo'
+import { WishlistProvider } from 'hooks/use-wishlist'
 
 function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
@@ -18,25 +21,26 @@ function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <CartProvider>
-            <Head>
-              <title>Won Games</title>
-              <link rel="shortcut icon" href="/img/icon-512.png" />
-              <link rel="apple-touch-icon" href="/img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-              <meta name="theme-color" content="#06092B" />
-              <meta
-                name="description"
-                content="The best game store in the world!"
+            <WishlistProvider>
+              <Head>
+                <title>Won Games</title>
+                <link rel="shortcut icon" href="/img/icon-512.png" />
+                <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta
+                  name="description"
+                  content="The best Game Stores in the world!"
+                />
+              </Head>
+              <GlobalStyles />
+              <NextNprogress
+                color="#F231A5"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={5}
               />
-            </Head>
-            <GlobalStyles />
-            <NextNprogress
-              color="#f231a5"
-              startPosition={0.3}
-              stopDelayMs={200}
-              height={3}
-            />
-            <Component {...pageProps} />
+              <Component {...pageProps} />
+            </WishlistProvider>
           </CartProvider>
         </ThemeProvider>
       </ApolloProvider>
