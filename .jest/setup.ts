36ded9ea5c3836cbe8d96a742db0387bd3ec3f8e
mizek1 <dannalves111@gmail.com>
2024-05-29 12:25:03 -0300

    test: configure msw and add tests to forgot and reset password forms

--- .jest/setup.ts
+++ .jest/setup.ts
@@ -1,2 +1,10 @@
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
+
+import dotenv from 'dotenv'
+
+dotenv.config({
+  path: '.env.development'
+})
+
+global.setImmediate = jest.useRealTimers as unknown as typeof setImmediate
